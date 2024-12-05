d687e79a587d9f1e9fc0b71ebcaede27b020f42a
gdblack69 <geetasaaroj@gmail.com>
2024-12-05 12:19:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,23 @@
 # config.py
-api_key = "nVIxy5BMLnEhi8oxC4"
-api_secret = "feIJsrtiXmFc0p4ll61saTYE83ssXSfu0bgc"
-api_hash = "b2dd9f27560e445abfe8f395d84ac3cf"
-bot_username = "@gpt3_unlim_chatbot"
-api_id = 22936737
-phone_number = 917897293175
+BYBIT_API_KEY = "nVIxy5BMLnEhi8oxC4"
+BYBIT_API_SECRET = "feIJsrtiXmFc0p4ll61saTYE83ssXSfu0bgc"
+API_HASH = "edbecdc187df07fddb10bcff89964a8e"
+BOT_USERNAME = "@gpt3_unlim_chatbot"
+API_ID = 14135677
+PHONE_NUMBER = 917897293175
+TEST_MODE = True  # Set to False for production
+LOG_LEVEL = "DEBUG"  # Optional: Adjust logging level to DEBUG, INFO, etc.
+
+# API credentials for source chat
+SOURCE_API_ID = 26697231
+SOURCE_API_HASH = '35f2769c773534c6ebf24c9d0731703a'
+SOURCE_CHAT_ID = -1002256615512
+
+# API credentials for destination account
+DESTINATION_API_ID = 14135677
+DESTINATION_API_HASH = 'edbecdc187df07fddb10bcff89964a8e'
+DESTINATION_BOT_USERNAME = '@gpt3_unlim_chatbot'
+
+# Paths for session files
+SOURCE_SESSION_FILE = "new16_source_session.session"
+DESTINATION_SESSION_FILE = "new16_destination_session.session"
